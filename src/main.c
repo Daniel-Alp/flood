1e071b6e4ffaef5fbceb135c0647ec17505c9115
Daniel Alp <daniel.alp.email@gmail.com>
2025-03-05 19:26:57 -0500

    implement scanning numbers, operators, parentheses

--- src/main.c
+++ src/main.c
@@ -1,11 +1,57 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+#include "scanner.h"
 
 int main() {
-    FILE *fp = fopen("./test//scanning/numbers.txt", "r");
-    int next;
-    if (fp) {
-        while((next = getc(fp)) != EOF) {
-            printf("%c",next);
-        }
+    FILE *fp = fopen("./test/scanning/numbers.txt", "rb");
+    char* buffer = NULL;
+    long length;
+    if (!fp) {
+        exit(1);
     }
+
+    fseek(fp, 0, SEEK_END);
+    length = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    buffer = malloc(length);
+    fread(buffer, 1, length, fp);
+
+    init_scanner(buffer);
+    struct Token token;
+    while ((token = next_token()).type != TOKEN_EOF) {
+        switch(token.type) {
+            case TOKEN_PLUS:
+                printf("PLUS");
+                break;
+            case TOKEN_MINUS:
+                printf("MINUS");
+                break;
+            case TOKEN_STAR:
+                printf("STAR");
+                break;
+            case TOKEN_SLASH:
+                printf("SLASH");
+                break;
+            case TOKEN_LEFT_PAREN:
+                printf("LEFT_PAREN");
+                break;
+            case TOKEN_RIGHT_PAREN:
+                printf("RIGHT_PAREN");
+                break;
+            case TOKEN_NUMBER:
+                printf("NUMBER");
+                break;
+            case TOKEN_ERROR:
+                printf("ERROR");
+                break;
+        }
+        printf(" ");
+        for (int i = 0; i < token.length; i++) {
+            printf("%c", token.start[i]);
+        }
+        printf("\n");
+    }
+
+    fclose(fp);
 }
---
+++ src/scanner.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <stdbool.h>
+
+enum TokenType {
+    TOKEN_PLUS, TOKEN_MINUS, TOKEN_STAR, TOKEN_SLASH,
+    TOKEN_LEFT_PAREN, TOKEN_RIGHT_PAREN,
+    TOKEN_NUMBER,
+    TOKEN_EOF,
+    TOKEN_ERROR
+};
+
+struct Token {
+    enum TokenType type;
+    const char *start;
+    int length;
+};
+
+struct Scanner {
+    const char *start;
+    const char *current;
+};
+
+void init_scanner(const char* src);
+
+bool is_at_end();
+
+char peek();
+
+char peek_next();
+
+char advance();
+
+void skip_ws();
+
+struct Token make_token(enum TokenType type);
+
+bool is_digit(char c);
+
+struct Token make_number();
+
+struct Token next_token();
