cd75f5ac618390d0d5f75ffcd943354b16434c44
Daniel Alp <daniel.alp.email@gmail.com>
2025-04-14 15:34:24 -0400

    remove old code

--- src/debug.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "parser.h"
-#include "compiler.h"
-
-void print_node(struct Node *node, u32 offset);
-
-void disassemble_chunk(struct Chunk *chunk);
--- src/error.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "parser.h"
-
-void error(struct Token token, const char *message, const char *source);
-
-void parse_error(struct Parser *parser, struct Token token, const char *message);
