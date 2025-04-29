3084800a80f3b8e8e54d2f2f01cb72874c174cc5
Daniel Alp <daniel.alp.email@gmail.com>
2025-04-29 14:58:52 -0400

    remove everything

--- src/debug.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "parse.h"
-#include "resolve.h"
-
-void print_node(struct Node *node, u32 offset);
-
-void print_ty(struct Ty *ty);
-
-void print_symtable(struct SymTable *st);
--- util/memory.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stddef.h>
-#include <stdlib.h>
-
-void *reallocate(void *ptr, size_t new_size);
-
-void *allocate(size_t size);
-
-void release(void *ptr);
